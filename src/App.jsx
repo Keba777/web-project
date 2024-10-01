fa5079635ddafedb910f58e4be1f7cda34bcf52d
Kibreab <Akkeba37@gmail.com>
2024-10-01 15:42:03 +0300

    Implement the NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import NavBar from "./components/NavBar"
+
 function App() {
   return (
     <div>
-      <h2 className='text-3xl text-blue-700'>Here is sample text</h2>
+      <NavBar/>
+      
     </div>
       
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const NavBar = () => {
   return (
-    <div></div>
-  )
-}
+    <nav className="py-11 px-10 text-white">
+      <div className="flex justify-between items-center">
+        {/* First section of the Nav */}
+        <ul className="flex space-x-8">
+          <li className="nav-item" >
+            What we believe
+          </li>
+          <li className="nav-item" >
+            Our features
+          </li>
+        </ul>
+        {/* Second section of the Nav */}
+        <ul className="flex space-x-8">
+          <li className="nav-item" >
+            Our story
+          </li>
+          <li className="nav-item" >
+            The waitlist
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
